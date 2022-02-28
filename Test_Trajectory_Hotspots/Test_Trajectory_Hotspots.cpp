885758c2d6c537e39ab19cfe542905917ddae9ce
Gert Meijer <gert_meijer89@hotmail.com>
2022-02-28 16:24:36 +0100

    initial commit with base project, test project, and github actions workflow.

---
+++ Test_Trajectory_Hotspots/Test_Trajectory_Hotspots.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Trajectory_Hotspots/Trajectory_Hotspots.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestTrajectoryHotspots
+{
+	TEST_CLASS(TestTrajectoryHotspots)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
